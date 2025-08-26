f3325db74c5766ef1c0af1f2f2878da0d9fa38f7
Frank Oliveira <fm0325836@gmail.com>
2025-08-26 16:17:15 -0300

    add Tarefa class with properties and methods for task management

--- ToDoList/src/br/edu/fatecpg/toDoList/model/Tarefa.java
+++ ToDoList/src/br/edu/fatecpg/toDoList/model/Tarefa.java
@@ -1,4 +1,57 @@
 package br.edu.fatecpg.toDoList.model;
 
 public class Tarefa {
+    public String id;
+    public String nome;
+    public String descricao;
+    public String status;
+
+    public Tarefa(String id, String nome, String descricao, String status) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
